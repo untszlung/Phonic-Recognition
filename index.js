f7c49b22d9bda439df7ee90780a62e120b3e86b5
Tsz Lung <mail@tszlung.com>
2021-01-07 09:56:05 +1100

    Update index.js

--- index.js
+++ index.js
@@ -19,39 +19,10 @@ const labels = [
   "_background_noise_"
 ];
 
-export const labelsSpa = [
-  "Limpiando dientes",
-  "Tosiendo",
-  "Teléfono sonando",
-  "Hablando",
-  "Tecleando",
-  "Ruido de fondo"
-];
 
-export const translate = labelEngils => {
-  if (labelEngils === "Brushing teeth") return labelsSpa[0];
-  if (labelEngils === "Coughing") return labelsSpa[1];
-  if (labelEngils === "Phone ringing") return labelsSpa[2];
-  if (labelEngils === "Speaking") return labelsSpa[3];
-  if (labelEngils === "Typing") return labelsSpa[4];
-  if (
-    labelEngils === "_background_noise_" ||
-    labelEngils === "Background noise"
-  )
-    return labelsSpa[5];
-};
 const lang = navigator.language || navigator.userLanguage;
 
-export const spanishMode = lang.includes("es") ? "es" : "en";
 
-if (spanishMode) {
-  document.getElementById("headerText").textContent =
-    "Identificador de sonidos";
-  document.getElementById("mainText").textContent =
-    "Este experimento necesita acceso a tu micrófono para predecir su actividad en función de las clases a la izquierda";
-  document.getElementById("start").textContent = "Empezar";
-  document.getElementById("closeControls").textContent = "Cerrar controles";
-}
 
 const predictionDiv = document.getElementsByClassName("prediction")[0];
 
@@ -69,19 +40,19 @@ startButton.onclick = () => {
     if (maximum > 0.7) {
       switch (maximum) {
         case data[0]:
-          currentPrediction = spanishMode ? labelsSpa[0] : labels[0];
+          currentPrediction = labels[0];
           break;
         case data[1]:
-          currentPrediction = spanishMode ? labelsSpa[1] : labels[1];
+          currentPrediction = labels[1];
           break;
         case data[2]:
-          currentPrediction = spanishMode ? labelsSpa[2] : labels[2];
+          currentPrediction = labels[2];
           break;
         case data[3]:
-          currentPrediction = spanishMode ? labelsSpa[3] : labels[3];
+          currentPrediction = labels[3];
           break;
         case data[4]:
-          currentPrediction = spanishMode ? labelsSpa[4] : labels[4];
+          currentPrediction = labels[4];
           break;
         default:
           currentPrediction = "";
