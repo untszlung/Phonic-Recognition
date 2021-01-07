0a9beee174ec3a9ae74c11de06d4a587b2e0f99d
Tsz Lung <mail@tszlung.com>
2021-01-07 11:22:43 +1100

    Change output class name

--- index.js
+++ index.js
@@ -11,12 +11,50 @@ let URL = `${window.location.href}/activities-model/`;
 setupBarGraph(URL);
 
 const labels = [
-  "Brushing teeth",
-  "Coughing",
-  "Phone ringing",
-  "Speaking",
-  "Typing",
-  "_background_noise_"
+  "Background Noise",
+  "OO",
+  "a",
+  "ai",
+  "ar",
+  "b",
+  "c_k",
+  "ch",
+  "d",
+  "e",
+  "ee",
+  "er",
+  "f",
+  "g",
+  "h",
+  "i",
+  "ie",
+  "j",
+  "l",
+  "m",
+  "n",
+  "ng",
+  "o",
+  "oa",
+  "oi",
+  "oo",
+  "or",
+  "ou",
+  "p",
+  "qu",
+  "r",
+  "s",
+  "sh",
+  "t",
+  "th_voiced",
+  "th_voiceless",
+  "u",
+  "ue",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "zh_(as_in_vision)"
 ];
 
 
@@ -53,6 +91,123 @@ startButton.onclick = () => {
           break;
         case data[4]:
           currentPrediction = labels[4];
+          break;
+        case data[5]:
+          currentPrediction = labels[5];
+          break;
+        case data[6]:
+          currentPrediction = labels[6];
+          break;
+        case data[7]:
+          currentPrediction = labels[7];
+          break;
+        case data[8]:
+          currentPrediction = labels[8];
+          break;
+        case data[9]:
+          currentPrediction = labels[9];
+          break;
+        case data[10]:
+          currentPrediction = labels[10];
+          break;
+        case data[11]:
+          currentPrediction = labels[11];
+          break;
+        case data[12]:
+          currentPrediction = labels[12];
+          break;
+        case data[13]:
+          currentPrediction = labels[13];
+          break;
+        case data[14]:
+          currentPrediction = labels[14];
+          break;
+        case data[15]:
+          currentPrediction = labels[15];
+          break;
+        case data[16]:
+          currentPrediction = labels[16];
+          break;
+        case data[17]:
+          currentPrediction = labels[17];
+          break;
+        case data[18]:
+          currentPrediction = labels[18];
+          break;
+        case data[19]:
+          currentPrediction = labels[19];
+          break;
+        case data[20]:
+          currentPrediction = labels[20];
+          break;
+        case data[21]:
+          currentPrediction = labels[21];
+          break;
+        case data[22]:
+          currentPrediction = labels[22];
+          break;
+        case data[23]:
+          currentPrediction = labels[23];
+          break;
+        case data[24]:
+          currentPrediction = labels[24];
+          break;
+        case data[25]:
+          currentPrediction = labels[25];
+          break;
+        case data[26]:
+          currentPrediction = labels[26];
+          break;
+        case data[27]:
+          currentPrediction = labels[27];
+          break;
+        case data[28]:
+          currentPrediction = labels[27];
+          break;
+        case data[29]:
+          currentPrediction = labels[29];
+          break;
+        case data[30]:
+          currentPrediction = labels[30];
+          break;
+        case data[31]:
+          currentPrediction = labels[31];
+          break;
+        case data[32]:
+          currentPrediction = labels[32];
+          break;
+        case data[33]:
+          currentPrediction = labels[33];
+          break;
+        case data[34]:
+          currentPrediction = labels[34];
+          break;
+        case data[35]:
+          currentPrediction = labels[35];
+          break;
+        case data[36]:
+          currentPrediction = labels[36];
+          break;
+        case data[37]:
+          currentPrediction = labels[37];
+          break;
+        case data[38]:
+          currentPrediction = labels[38];
+          break;
+        case data[39]:
+          currentPrediction = labels[39];
+          break;
+        case data[40]:
+          currentPrediction = labels[40];
+          break;
+        case data[41]:
+          currentPrediction = labels[41];
+          break;
+        case data[42]:
+          currentPrediction = labels[42];
+          break;
+        case data[43]:
+          currentPrediction = labels[43];
           break;
         default:
           currentPrediction = "";
